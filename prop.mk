29828b9457539da81f45c5ceea40a4c89535bf9a
rago <rago@google.com>
2016-07-01 16:05:00 -0700

    mido: Set voice call volume steps to 7
    Bug: 28426597
    Change-Id: Icf78d9acec75c500982b1e4391af4d6f3933da37

--- prop.mk
+++ prop.mk
@@ -17,6 +17,7 @@ persist.vendor.btstack.enable.splita2dp=false \
 ro.af.client_heap_size_kbyte=7168 \
 ro.vendor.audio.sdk.fluencetype=fluence \
 ro.vendor.audio.sdk.ssr=false \
+ro.config.vc_call_vol_steps=7 \
 vendor.audio.flac.sw.decoder.24bit=true \
 vendor.audio.offload.buffer.size.kb=64 \
 vendor.audio.offload.gapless.enabled=true \
