bdf63c760ef98ef540b06f92bea9d9041f3abee9
Artem Borisov <dedsa2002@gmail.com>
2018-11-07 12:25:37 +0400

    mido: Enable USB via ConfigFS
    Required on 4.9 now.
    
    Change-Id: I1b4557da497c3c63d64770f94a2ff56e72602f8a
    [kras: take into account eng deprecation]
    Signed-off-by: Albert I <kras@raphielgang.org>

--- device.mk
+++ device.mk
@@ -306,6 +306,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.goodix.sh \
     init.insmod.sh \
+    init.msm.usb.configfs.rc \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
