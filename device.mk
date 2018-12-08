14891128c66705f2ed13f777619c7119d415f3fa
kondors1995 <normandija1945@gmail.com>
2018-12-08 19:29:59 +0200

    Add power-libpermgr (EAS 1.2 Power HAL )
    Squashed commit of the following:
    
    commit dd64e5f4d726f9c24fd722215ee27724e72d80d9
    Author: Wei Wang <wvw@google.com>
    Date:   Mon Jun 25 14:23:49 2018 -0700
    
        power: skip Audio streaming hint in SUSTAINED_PERFORMANCE mode
    
        Bug: 110795217
        Test: Boot
        Change-Id: I2e005ccefce8535f59d6b88e6367fe63bd6f7355
        Signed-off-by: DennySPB <dennyspb@gmail.com>
    
    commit 7e4e7cbf2c0307409d2fa6e406895f0f72419a7a
    Author: Wei Wang <wvw@google.com>
    Date:   Fri May 25 18:22:35 2018 -0700
    
        power: make powerhal starts handling powerhint after boot_complete
    
        some sysfs node may not be ready before boot_complete:
        e.g. we are delaying bw_hwmon in favor of boot time
    
        Bug: 80321544
        Bug: 79536745
        Test: Boot and check powerhint handling
        Change-Id: I8d2cfa332d9cf9fbafdfa3f48ffff4105c6bcaae
        Signed-off-by: DennySPB <dennyspb@gmail.com>
    
    commit 990b39bb2b0d1fc9b7613463e1a4d8eaeece745a
    Author: Demon Singur <demonsingur@gmail.com>
    Date:   Thu Oct 25 04:39:35 2018 +0000
    
        power: clean up
    
        Change-Id: I72d4e2022db65863f3e5fdc11784ed2068714dcf
        Signed-off-by: DennySPB <dennyspb@gmail.com>
    
    commit 16dfc76181c1b7023227f1473da2b3f3cc44e187
    Author: Vol Zhdanov <wight554@gmail.com>
    Date:   Tue Sep 11 13:54:20 2018 +0000
    
        update power-libpermgr
    
        * remove vr hints from powerhal and powerhint
        * reduce schetune boost to 20
    
        Change-Id: I84ec55bd0db6b69939ff80daabc2e368b735574e
        Signed-off-by: DennySPB <dennyspb@gmail.com>
    
    commit 2e2aa5e26249c11951ee309b1b7dc90de5fd9358
    Author: Rashed97 <rashed@linux.com>
    Date:   Tue Dec 4 11:31:25 2018 +0300
    
        power-libperfmgr: Return empty subsystems power stats conditionally
    
        just return an empty subsystems supported count for devices that do not have wifi power stats
    
        Change-Id: Iefadaf07c4f13275c2880013f05278e2fd404ab3
        Signed-off-by: DennySPB <dennyspb@gmail.com>
    
    commit bb956102818aee5cb2047da1d1842c62583d7db7
    Author: DennySPB <dennyspb@gmail.com>
    Date:   Tue Dec 4 11:20:45 2018 +0300
    
        power-libperfmgr: convert .bp to .mk
    
        this needed for adding custom flags without soong changes
    
        Change-Id: Ic571f7aa0a01940f0ea1d018a2ec4202b7291a1b
        Signed-off-by: DennySPB <dennyspb@gmail.com>
    
    commit e1ddaad17b043da62ec57f6256a53998c19ec552
    Author: Davide Garberi <dade.garberi@gmail.com>
    Date:   Sun Dec 2 22:10:25 2018 +0100
    
        Revert "Add temp workaround Easel power stats on 2017"
    
        This reverts commit b296bbf7f09d5b73e5f14abcb1bd5b9668ac0074.
    
        Change-Id: I534e8acd99639a6b81862417d1b98a2bb21161a0
        Signed-off-by: DennySPB <dennyspb@gmail.com>
    
    commit 20d0a1994e0d74993b31d0509f399e47324e5c0a
    Author: Davide Garberi <dade.garberi@gmail.com>
    Date:   Sun Dec 2 20:51:38 2018 +0100
    
        Revert "power: add dumpstate support in Power HAL"
    
        This reverts commit e671f7473c1d63207ef268e48f315c31a16c5103.
    
        Change-Id: I5b37e408617c07cbcff166ad8492b9768d299e83
        Signed-off-by: DennySPB <dennyspb@gmail.com>
    
    commit d9db09280961bae155f6ab46ed065ce53b84b704
    Author: Davide Garberi <dade.garberi@gmail.com>
    Date:   Sun Dec 2 20:42:22 2018 +0100
    
        Add a powerhint.json
    
        * All the values have been manually written crosschecking wahoo's values and the dts
    
        Change-Id: I0e010ca45155758159de7b9be016ed5f6f16a464
        Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
        Signed-off-by: DennySPB <dennyspb@gmail.com>
    
    commit f55dba64a2cc507e64222b1ddfd5882183095fff
    Author: Vol Zhdanov <wight554@gmail.com>
    Date:   Mon Sep 10 18:42:52 2018 +0000
    
        Switch to power-libperfmgr
    
        * cleanup unused camera hints
        * revert b296bbf
    
        Change-Id: I6a52a8d0bd527b153dc9558670366b14667803e5
        Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
        Signed-off-by: DennySPB <dennyspb@gmail.com>

--- device.mk
+++ device.mk
@@ -282,10 +282,14 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service.msm8953-libperfmgr
 
-PRODUCT_COPY_FILES += \
+#PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
+#Powerhint EAS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # QMI
 PRODUCT_PACKAGES += \
