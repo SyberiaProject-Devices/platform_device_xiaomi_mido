657bfd74e2f86ea418326aac451fdf43dd626981
Michael Bestas <mkbestas@lineageos.org>
2019-01-19 21:17:27 +0200

    mido: bluetooth: Update bluetooth properties for pie
    Change-Id: Id354a0dfa1ced0e0ec2a1904205a8d50253d606a

--- prop.mk
+++ prop.mk
@@ -43,8 +43,7 @@ vendor.voice.voip.conc.disabled=true
 PRODUCT_PROPERTY_OVERRIDES += \
 bluetooth.hfp.client=1 \
 vendor.qcom.bluetooth.soc=smd \
-ro.bluetooth.hfp.ver=1.7 \
-ro.qualcomm.bt.hci_transport=smd
+ro.bluetooth.hfp.ver=1.7
 
 # Boot
 PRODUCT_PROPERTY_OVERRIDES += \
