426cc31f4c4d4aa21c2c12cb91dafd01bf12205c
kondors1995 <normandija1945@gmail.com>
2019-01-26 10:14:13 +0000

    update display blobs from from LA.UM.7.6.2.r1-03000-89xx.0

--- device.mk
+++ device.mk
@@ -161,6 +161,12 @@ PRODUCT_PACKAGES += \
     libgenlock \
     libtinyxml
 
+PRODUCT_PACKAGES += \
+    vendor.display.color@1.0-service \
+    vendor.display.color@1.0-impl \
+    vendor.display.config@1.1 \
+    vendor.display.config@1.1_vendor
+
 # Doze mode
 #PRODUCT_PACKAGES += \
 #    XiaomiDoze
