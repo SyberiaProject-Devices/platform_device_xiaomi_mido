a4056e90889480701c81d95fdc98834636db81cf
kondors1995 <normandija1945@gmail.com>
2019-03-01 09:28:30 +0000

    Add hostapd configs needed for CAF wlan Kanged from tissot-user-9-PKQ1.180917.001-V10.0.5.0.PDHMIXM-release-keys

--- device.mk
+++ device.mk
@@ -405,16 +405,23 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
-# AdvancedControls
-PRODUCT_PACKAGES += \
-    AdvancedControls \
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/hostapd/hostapd.accept:system/etc/hostapd/hostapd.accept \
+    $(LOCAL_PATH)/wifi/hostapd/hostapd.deny:system/etc/hostapd/hostapd.deny \
+    $(LOCAL_PATH)/wifi/hostapd/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf
+
+
 # Wi-Fi Display
 PRODUCT_BOOT_JARS += \
     WfdCommon
+
+# AdvancedControls
+PRODUCT_PACKAGES += \
+    AdvancedControls \
