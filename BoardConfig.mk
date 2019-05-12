2585dc073b00abe935303e7cf5368940873812e9
Silesh K Nair <sileshn@gmail.com>
2019-05-12 07:30:19 +0530

    mido: remove more traces of common repo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -176,8 +176,8 @@ DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 HWUI_COMPILE_FOR_PERF := true
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_msm8953
-TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8953
+TARGET_INIT_VENDOR_LIB := libinit_mido
+TARGET_RECOVERY_DEVICE_MODULES := libinit_mido
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
