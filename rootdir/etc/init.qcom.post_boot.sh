d3cfc46bdbba80e59d087c9ad01bb6831f5daa18
kondors1995 <normandija1945@gmail.com>
2019-08-22 08:10:01 +0000

    mido:post_boot: Adapt for CFS since we use conservative for CFS lets adapt HMP sectionf or it

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -102,19 +102,11 @@ function 8953_sched_dcvs_hmp()
 
     #governor settings
     echo 1 > /sys/devices/system/cpu/cpu0/online
-    echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-    echo "19000 1401600:39000" > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
-    echo 85 > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
-    echo 20000 > /sys/devices/system/cpu/cpufreq/interactive/timer_rate
-    echo 1401600 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
-    echo 0 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
-    echo "85 1401600:80" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
-    echo 39000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
-    echo 40000 > /sys/devices/system/cpu/cpufreq/interactive/sampling_down_factor
+    echo "conservative" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+    echo "85" > /sys/devices/system/cpu/cpufreq/conservative/up_threshold
+    echo "30" > /sys/devices/system/cpu/cpufreq/conservative/down_threshold
     echo 19 > /proc/sys/kernel/sched_upmigrate_min_nice
     # Enable sched guided freq control
-    echo 1 > /sys/devices/system/cpu/cpufreq/interactive/use_sched_load
-    echo 1 > /sys/devices/system/cpu/cpufreq/interactive/use_migration_notif
     echo 200000 > /proc/sys/kernel/sched_freq_inc_notify
     echo 200000 > /proc/sys/kernel/sched_freq_dec_notify
 
