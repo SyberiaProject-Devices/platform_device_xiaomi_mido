7b3548339db0f47bff99354aeedbc25959678ac2
ZeeLog <Danjel.Zammit@gmail.com>
2020-05-14 11:26:15 +0200

    mido: Add GraphicBuffer shim for camera
    * Address errors related to cannot locate symbol
      "_ZN7android13GraphicBufferC1EjjijjP13native_handleb"
      referenced by "/vendor/lib/libmmcamera_ppeiscore.so"...
    
    Change-Id: I73a6e85de78f3f35cb421a52666f8ffa3dbd0898

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -190,6 +190,11 @@ include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
+# Shims
+TARGET_LD_SHIM_LIBS := \
+/vendor/lib/hw/camera.msm8953.so|libshim_camera.so \
+/vendor/lib/libmmcamera_ppeiscore.so|libshim_camera.so
+
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 PRODUCT_FULL_TREBLE_OVERRIDE := true
