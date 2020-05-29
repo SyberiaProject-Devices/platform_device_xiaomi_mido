2254a464d045cca97eb4ef538c1487a102a3c9ca
dianlujitao <dianlujitao@gmail.com>
2020-05-29 15:35:21 +0300

    Switch to QTI bt stack and enable ad2p offload
    Change-Id: I685862c842e351646302f10eb51a8076c4ebe531
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- prop.mk
+++ prop.mk
@@ -222,3 +222,10 @@ wifi.interface=wlan0
 # Wifi-Display
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.sys.wfd.virtual=0
+
+# QTI Bluetooth
+ro.bluetooth.library_name=libbluetooth_qti.so
+vendor.bluetooth.soc=cherokee
+persist.vendor.btstack.enable.splita2dp=true
+persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac
+persist.vendor.btstack.enable.twsplus=true
